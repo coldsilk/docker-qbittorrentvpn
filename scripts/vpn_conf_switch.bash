05cadc9332860057d75bac9148bef7e5f3171a79
coldsilk <60486751+coldsilk@users.noreply.github.com>
2024-02-26 14:23:25 -0500

    Create vpn_conf_switch.bash to switch confs on failed health check
    Added a script to switch confs when a health check fails.

---
+++ scripts/vpn_conf_switch.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+function vpn_conf_switch() (
+  # Fill in VPN_TYPE and CONFIG_DIR
+  # Create directory "$CONFIG_DIR/openvpn_confs" or "$CONFIG_DIR/wireguard_confs"
+  #   then put all of your VPN config files in their respective confs directory.
+  # If using openvpn, you must use the filename "default.ovpn"
+  #   ie. /config/openvpn/default.opvn
+  #   Same principle as when using wireguard that you must use "/config/wireguard/wg0.conf"
+  # VPN conf files are rotated based on the oldest modification time.
+      VPN_TYPE="${1,,}"
+    CONFIG_DIR="/config"
+     CONFS_DIR="$CONFIG_DIR/${VPN_TYPE}_confs"
+  CONFS_FILTER='ls -tp1 "$CONFS_DIR" | grep -v /' # eval'd
+  PRINT_PREFIX="$(basename "$0")"
+
+  if [ ! -d "$CONFS_DIR" ]; then
+    echo "$PRINT_PREFIX: ERROR: VPN confs directory doesn't exist: \"$CONFS_DIR\""
+    return 1
+  fi
+
+  if [ "$VPN_TYPE" != "openvpn" ] && [ "$VPN_TYPE" != "wireguard" ]; then
+    echo "$PRING_PREFIX: ERROR: VPN_TYPE is not \"openvpn\" or \"wireguard\": \"$VPN_TYPE\""
+    return 2
+  fi
+
+  next_vpn_conf=""
+  # next_vpn_conf is set to the last line of the output
+  while IFS= read line ; do
+      next_vpn_conf=$line;
+  done < <(eval "$CONFS_FILTER");
+
+  if [ ! -f "$CONFS_DIR/$next_vpn_conf" ]; then
+    echo "$PRINT_PREFIX: ERROR: confs directory seems to have no files: \"$CONFS_DIR\""
+    return 3
+  fi
+
+  vpn_file="default.ovpn"
+  if [ "$VPN_TYPE" == "wireguard" ]; then
+    vpn_file="wg0.conf";
+  fi
+  echo "$PRINT_PREFIX: copying VPN file \"$CONFS_DIR/$next_vpn_conf\" to \"$CONFIG_DIR/$VPN_TYPE/$vpn_file\""
+  cp -f "$CONFS_DIR/$next_vpn_conf" "$CONFIG_DIR/$VPN_TYPE/$vpn_file"
+  touch "$CONFS_DIR/$next_vpn_conf"
+  return 0
+)
+# determine if the script was sourced or not, sourced == true
+if ! (return 0 2>/dev/null); then
+  vpn_conf_switch "$@";
+  exit $?;
+fi
