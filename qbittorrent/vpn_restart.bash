ef68cdf662b9046b442f8c01f3cff676edfc8ef8
coldsilk <60486751+coldsilk@users.noreply.github.com>
2024-02-28 15:31:03 -0500

    Add files via upload

---
+++ qbittorrent/vpn_restart.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+vpn_pid=""
+if [ "openvpn" == "${VPN_TYPE}" ]; then
+  vpn_pid=$(echo $(ps -aux | grep "openvpn" | grep -v "grep") | cut -d ' ' -f 2 | grep "^[0-9]\+$");
+elif [ "wireguard" == "${VPN_TYPE}" ]; then
+# 1248 ?        00:00:00 wg-crypt-wgnet1
+  vpn_pid=$(echo $(ps -e | grep "wg-" | grep -v "grep") | cut -d ' ' -f 1 | grep "^[0-9]\+$");
+else
+  echo "[ERROR] $(basename "$0"): VPN type not understood, received: ${VPN_TYPE}" | ts '%Y-%m-%d %H:%M:%.S'
+fi
+if [ "$vpn_pid" != "" ]; then
+  kill -9 $vpn_pid
+  sleep 1
+  /scripts/vpn_conf_switch.sh "${VPN_TYPE}"
+  echo "[INFO] $(basename "$0"): Restarting ${VPN_TYPE}..." | ts '%Y-%m-%d %H:%M:%.S'
+  /etc/qbittorrent/vpn_start.bash
+fi
+
---
+++ qbittorrent/vpn_start.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "openvpn" == "${VPN_TYPE}" ]; then
+  cd /config/openvpn
+  exec openvpn --pull-filter ignore route-ipv6 --pull-filter ignore ifconfig-ipv6 --config "${VPN_CONFIG}" &
+elif [ "wireguard" == "${VPN_TYPE}" ]; then
+  cd /config/wireguard
+  if ip link | grep -q `basename -s .conf $VPN_CONFIG`; then
+    wg-quick down $VPN_CONFIG || echo "[INFO] $(basename "$0"): WireGuard is down already" | ts '%Y-%m-%d %H:%M:%.S' # Run wg-quick down as an extra safeguard in case WireGuard is still up for some reason
+    sleep 0.5 # Just to give WireGuard a bit to go down
+  fi
+  wg-quick up $VPN_CONFIG
+else
+  echo "[ERROR] $(basename "$0"): VPN type not understood, received: ${VPN_TYPE}" | ts '%Y-%m-%d %H:%M:%.S'
+fi
