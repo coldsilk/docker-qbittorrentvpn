eff9dd94d208077c81049fd3703af3bb727d2e6f
coldsilk <60486751+coldsilk@users.noreply.github.com>
2024-04-01 08:03:18 -0400

    Added double quotes

--- qbittorrent/vpn_start.bash
+++ qbittorrent/vpn_start.bash
@@ -4,11 +4,12 @@ if [ "openvpn" == "${VPN_TYPE}" ]; then
   exec openvpn --pull-filter ignore route-ipv6 --pull-filter ignore ifconfig-ipv6 --config "${VPN_CONFIG}" &
 elif [ "wireguard" == "${VPN_TYPE}" ]; then
   cd /config/wireguard
-  if ip link | grep -q `basename -s .conf $VPN_CONFIG`; then
-    wg-quick down $VPN_CONFIG || echo "[INFO] $(basename "$0"): WireGuard is down already" | ts '%Y-%m-%d %H:%M:%.S' # Run wg-quick down as an extra safeguard in case WireGuard is still up for some reason
+  if ip link | grep -q `basename -s .conf "$VPN_CONFIG"`; then
+    # Run wg-quick down as an extra safeguard in case WireGuard is still up for some reason
+    wg-quick down "$VPN_CONFIG" || echo "[INFO] $(basename "$0"): WireGuard is down already" | ts '%Y-%m-%d %H:%M:%.S'
     sleep 0.5 # Just to give WireGuard a bit to go down
   fi
-  wg-quick up $VPN_CONFIG
+  wg-quick up "$VPN_CONFIG"
 else
   echo "[ERROR] $(basename "$0"): VPN type was not understood, received: ${VPN_TYPE}" | ts '%Y-%m-%d %H:%M:%.S'
 fi
