d0e1b099be221d85d98ff7c23f99e41f7b35ad6b
coldsilk <60486751+coldsilk@users.noreply.github.com>
2024-04-20 15:48:44 -0400

    Update set_interface.bash

--- qbittorrent/set_interface.bash
+++ qbittorrent/set_interface.bash
@@ -9,7 +9,7 @@ qbittorrent_conf_set_interface() {
   if cat "$1" | grep -qF '[BitTorrent]'; then
     local adapter="$(netstat -ie | grep -o ".*wg.*: flags=\|.*tun.*: flags=\|.*tap.*: flags=" | cut -d ':' -f 1)"
     if [ "" == "$adapter" ]; then
-      eprint ""$(basename "$0")": Could not find VPN adapter with command 'netstat -ie'. Cannot set interface in $1"
+      eprint "$(basename "$0"): Could not find VPN adapter with command 'netstat -ie'. Cannot set interface in $1"
       return 1
     fi
     if cat "$1" | grep -qF 'Session\Interface='; then
@@ -27,7 +27,7 @@ qbittorrent_conf_set_interface() {
       sed -i "s/^\[BitTorrent\]$/[BitTorrent\]\nSession\\\InterfaceName=$adapter/" "$1"
     fi
   else
-    eprint ""$(basename "$0")": The section [BitTorrent] wasn't found in $1"
+    eprint "$(basename "$0"): The section [BitTorrent] wasn't found in $1"
     return 1;
   fi
   # I'm not sure if Connection\Interface is still observed
@@ -48,9 +48,16 @@ qbittorrent_conf_set_interface() {
       sed -i "s/^\[Preferences\]$/[Preferences\]\nConnection\\\InterfaceName=$adapter/" "$1"
     fi
   else
-    eprint ""$(basename "$0")": The section [Preferences] wasn't found in $1"
+    eprint "$(basename "$0"): The section [Preferences] wasn't found in $1"
+    return 1
   fi
-  return 0
+  if cat "$1" | grep -qF "Session\Interface=$adapter" \
+  && cat "$1" | grep -qF "Session\InterfaceName=$adapter" \
+  && cat "$1" | grep -qF "Connection\Interface=$adapter" \
+  && cat "$1" | grep -qF "Connection\InterfaceName=$adapter";
+  then return 0; fi
+  eprint "$(basename "$0"): sed failed to write interface lines in $1"
+  return 1
 }
 
 # Determines if the script was sourced or not.
